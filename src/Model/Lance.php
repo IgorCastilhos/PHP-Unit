81a2f5f0e46fe85698b8591dd670182dfa8e9fcb
igorc <igorcastilhos2010@hotmail.com>
2024-08-18 20:45:43 -0300

    start: First Commit

---
+++ src/Model/Lance.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Alura\Leilao\Model;
+
+class Lance
+{
+    /** @var Usuario */
+    private $usuario;
+    /** @var float */
+    private $valor;
+
+    public function __construct(Usuario $usuario, float $valor)
+    {
+        $this->usuario = $usuario;
+        $this->valor = $valor;
+    }
+
+    public function getUsuario(): Usuario
+    {
+        return $this->usuario;
+    }
+
+    public function getValor(): float
+    {
+        return $this->valor;
+    }
+}
---
+++ src/Service/Avaliador.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Alura\Leilao\Service;
+
+use Alura\Leilao\Model\Leilao;
+
+class Avaliador
+{
+    private float $maiorValor = 0;
+
+    public function avalia(Leilao $leilao): void
+    {
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+        }
+    }
+
+    /**
+     * @return float
+     */
+    public function getMaiorValor(): float
+    {
+        return $this->maiorValor;
+    }
+}
