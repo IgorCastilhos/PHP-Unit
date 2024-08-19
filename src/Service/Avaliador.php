650f3ebffb47a023e2aec1663941022205552ae2
igorc <igorcastilhos2010@hotmail.com>
2024-08-19 00:33:03 -0300

    add: adiciona teste para encontrar o menor valor de lances em ordem decrescente e encontrar o menor valor em ordem crescente

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -6,13 +6,18 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private float $maiorValor = 0;
+    private float $maiorValor = -INF;
+    private float $menorValor = INF;
 
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
+            }
+
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
             }
         }
     }
@@ -23,5 +28,13 @@ class Avaliador
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
     }
 }
